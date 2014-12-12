f3b87007531cd9bc9d1490b40ae78460fe3efaaf
lugearma <lugearma@gmail.com>
2014-12-12 12:32:38 -0600

    Listo, ya vio a bootstrap

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(session({
 app.use(lessMiddleware(__dirname + 'public'));
 
 //Cargamos los archivos estaticos
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 // app.use(function (req, res, next){
 // 	res.locals._csrf = req.session._csrf;
